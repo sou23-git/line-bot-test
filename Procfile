5f134d26184a0c1d459b0c7f1ebc3b4115fdbb84
sou23-git <sooou23ohata@gmail.com>
2023-03-04 02:31:59 +0900

    Added 2 files and testing weather-get.py

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:python=main.py
+web:python=weather-get.py
--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from flask import Flask, request, abort
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage,
-)
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
+
 import os
 
 app = Flask(__name__)
