607ae70bba4185b28c18b70b2de925d86f729a8e
sou23-git <sooou23ohata@gmail.com>
2023-03-04 02:48:05 +0900

    Renamed and testing "getting weather"

--- main.py
+++ main.py
@@ -1,73 +1,54 @@
-from flask import Flask, request, abort
-
-from linebot import LineBotApi, WebhookHandler
+from flask import Flask,request,abort
+from linebot import LineBotApi,WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
-
+from linebot.models import MessageEvent,TextMessage,TextSendMessage
 import os
+import requests
+import pprint
+import urllib.request
+from bs4 import BeautifulSoup
 
-app = Flask(__name__)
 
-# チャネルシークレットを設定
-YOUR_CHANNEL_SECRET = "63d4fc0f5af7948c6658ab08b6a32c4b"
-# チャネルアクセストークンを設定
-YOUR_CHANNEL_ACCESS_TOKEN = "C/R9zrByu/3DDFMEaGI8gV+XQePJV1ZC5ApLbHzVBuhqNCMLjMLrL6s+MuZs+Prl6Qu1hh9LdSlgz746Xzfaa8pnTeweyb5FeSHFByr+m31IcHUVLQZw5agbb+swmH2GnRx7mO8n8yOadl6rt7GHdwdB04t89/1O/w1cDnyilFU="
+import scw    #先ほどファイルをインポート
 
-line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+app=Flask(__name__)
+#環境変数の取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["C/R9zrByu/3DDFMEaGI8gV+XQePJV1ZC5ApLbHzVBuhqNCMLjMLrL6s+MuZs+Prl6Qu1hh9LdSlgz746Xzfaa8pnTeweyb5FeSHFByr+m31IcHUVLQZw5agbb+swmH2GnRx7mO8n8yOadl6rt7GHdwdB04t89/1O/w1cDnyilFU="]
+YOUR_CHANNEL_SECRET = os.environ["63d4fc0f5af7948c6658ab08b6a32c4b"]
+line_bot_api=LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler=WebhookHandler(YOUR_CHANNEL_SECRET)
 
-# https://example.herokuapp.com/callback にアクセスされたら以下の関数を実行する
-@app.route("/callback", methods=['POST'])
+#テスト用
+@app.route("/")
+def hello_world():
+   return "cheer up!!2"
+
+@app.route("/callback",methods=["POST"])
 def callback():
-    # アクセス時に送られてきたデータ「X-Line-Signature」を代入
-    signature = request.headers['X-Line-Signature']
+    signature=request.headers["X-Line-Signature"]
 
-    # アクセス時に送られてきたデータの主な部分を代入
-    body = request.get_data(as_text=True)
+    body=request.get_data(as_text=True)
+    app.logger.info("Request body"+body)
 
-    # try 内でエラーが発生したら except の文を実行
     try:
-        # ハンドラーに定義されている関数を呼び出す
-        handler.handle(body, signature)
-    # もし「InvalidSigunatureError」というエラーが発生したら、以下のプログラムを実行
+        handler.handle(body,signature)
     except InvalidSignatureError:
-        # リクエストを送った側に400番(悪いリクエストですよー！)を返す
         abort(400)
-
-    # すべて順調にいけば、リクエストを送った側に「OK」と返す
     return "OK"
 
-# ハンドラーに定義されている関数
-@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent,message=TextMessage)
 def handle_message(event):
-    # ここにメッセージの内容による処理を書いていこう
+    #入力された文字を取得
+    text_in = event.message.text
 
-    # メッセージの種類が「テキスト」なら
-    if event.type == "message":
-        response_message = ""
+    if "今日" in text_in:   #scw.pyのgetw関数を呼び出している
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=scw.getw()))
+    elif "明日" in text_in:   #scw.pyのtom_getw関数を呼び出している
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=scw.tom_getw()))
+    else:   #「今日」「明日」以外の文字はオウム返しする
+     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
 
-        # event.message.text という変数にメッセージの内容が入っている
-        if (event.message.text == "おはようございます"):
-            response_message = "Good morning!"
 
-        elif (event.message.text == "こんにちは"):
-            response_message = "Good afternoon!"
-
-        elif (event.message.text == "こんばんは"):
-            response_message = "Good evening!"
-
-        else:
-            response_message = "その言葉はわかりません。"
-
-        # 返信文を送信
-        # response_message の中に入っている文を返す
-        line_bot_api.reply_message(
-            event.reply_token,
-            [
-                TextSendMessage(text=response_message)
-            ]
-        )
-
-# ポート番号を環境変数から取得
-port = os.getenv("PORT")
-app.run(host="0.0.0.0", port=port)
+if __name__=="__main__":
+    port=int(os.getenv("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
